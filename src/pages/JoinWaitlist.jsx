10cb6987f87ced0f9511b19c95b7c8c4a7e79925
Sticklo <mafimisebiadebayo@gmail.com>
2022-03-31 22:27:01 +0100

    typography fix for mobile devices

--- src/pages/JoinWaitlist.jsx
+++ src/pages/JoinWaitlist.jsx
@@ -20,7 +20,7 @@ function JoinWaitlist() {
         <div className="flex flex-col items-center gap-y-3.5 lg:flex-row lg:justify-between lg:items-center ">
           <div className="flex flex-col items-center lg:items-start  w-[7.912rem]">
             <img src={Bolt} alt="Bolt" className="w-[3rem] mb-[1.5rem]" />
-            <h1 className="text-center lg:text-left font-bold text-[1.8rem] leading-[2.24rem] mb-[1.5rem]">
+            <h1 className="text-center lg:text-left font-bold text-[1.4rem] md:text-[1.8rem] leading-[2.24rem] mb-[1.5rem]">
               Join the waitlist
             </h1>
             <p className="font-bold text-[0.8rem]">
@@ -29,7 +29,7 @@ function JoinWaitlist() {
           </div>
 
           <form className="bg-[#161823] flex flex-col lg:w-[30.912rem] pt-[1.75rem] pb-[5.316rem] px-[4rem] rounded-[5px]">
-            <h1 className="font-bold text-[1.8rem] leading-[4.24rem]">
+            <h1 className="font-bold text-[1.5rem] md:text-[1.8rem] leading-[4.24rem]">
               Get early access
             </h1>
             <div className="flex flex-col gap-3">
